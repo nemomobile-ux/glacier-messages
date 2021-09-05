db0260c45d5d0e9d7f6bfeb858671761b4d0d0fc
Jozef Mlich <jmlich83@gmail.com>
2021-09-05 18:21:19 +0000

    use cmake, add translations

---
+++ translations/glacier-messages.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ChatTextInput</name>
+    <message>
+        <location filename="../qml/components/ChatTextInput.qml" line="85"/>
+        <source>Type a message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ChatTextInput.qml" line="122"/>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConversationListPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationListPage.qml" line="44"/>
+        <source>Messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConversationListWidget</name>
+    <message>
+        <location filename="../qml/components/ConversationListWidget.qml" line="44"/>
+        <source>No messages yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConversationPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationPage.qml" line="92"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="49"/>
+        <source>Messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/glacier-messages_cs.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>ChatTextInput</name>
+    <message>
+        <location filename="../qml/components/ChatTextInput.qml" line="85"/>
+        <source>Type a message</source>
+        <translation>Napište zprávu</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ChatTextInput.qml" line="122"/>
+        <source>Send</source>
+        <translation>Odeslat</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationListPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationListPage.qml" line="44"/>
+        <source>Messages</source>
+        <translation>Zprávy</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationListWidget</name>
+    <message>
+        <location filename="../qml/components/ConversationListWidget.qml" line="44"/>
+        <source>No messages yet</source>
+        <translation>Zatím žádné zprávy</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationPage.qml" line="92"/>
+        <source>Phone number</source>
+        <translation>Telefonní číslo</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="49"/>
+        <source>Messages</source>
+        <translation>Zprávy</translation>
+    </message>
+</context>
+</TS>
